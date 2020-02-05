56a3d0ff9e249657c3180af957d84635192849d3
nicolas jesenberger <nicolas@jesenberger.fr>
2020-02-05 14:50:41 +0100

    customization css

--- src/js/reload.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-    window.addEventListener("resize",function(){
-        var container = document.querySelector('.reload');
-        var content = container.innerHTML;
-        container.innerHTML= content; 
-    });
-
-});
